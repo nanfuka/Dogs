4bac69dad5a6c88ace457c48bebce87b39ca1460
nanfuka <kalungi2k6@gmail.com>
2018-11-08 17:01:25 +0300

    Add pets_class.py

--- pets_class.py
+++ pets_class.py
@@ -1,32 +1,35 @@
-class Pets:
-    species = 'mammal'
+class Dog:
     def __init__(self, name, age):
+        self.is_hungry = False
         self.name = name
         self.age = age
-      
-pets = Pets('Tom', 6)
-print("I have 3 dogs.")
-print("{} is {}.".format(pets.name, pets.age))
 
-pet = Pets('Fletcher', 7)
-print("{} is {}.".format(pet.name, pets.age))
-
-pet = Pets('Larry', 9)
-print("{} is {}.".format(pet.name, pet.age))
-print("And they're all {} of course.".format(pet.species))
-
-class Dog(Pets):
-    def __init__(self):
-        self.is_hungry = True
     def eat(self):
-        if self.is_hungry:
-            return "My dogs are not hungry"
-        return "My dogs are hungry"
-    def walk(self):
-        return "%s is walking!" % (self.name)
-        
-dog = Dog()
-print(dog.walk())
+        self.is_hungry = True
 
 
-        
+class Pets:
+    dogs =[]
+    species = 'mammal'
+    def __init__(self, dogs):
+        self.dogs = dogs
+mypets = [Dog('Tom', 6), Dog('Fletcher', 7), Dog('Larry', 9)]       
+      
+pets = Pets(mypets)
+
+
+
+print("I have {} dogs".format(len(pets.dogs)))
+
+for dog in pets.dogs:
+    print("{} is {}.".format(dog.name, dog.age))
+
+    dog.eat()
+x= False
+for dog in pets.dogs:
+    if dog.is_hungry:
+        x=True
+if x == False:
+    print ("my dogs are hungry") 
+else:
+    print("my dogs are not hungry") 
